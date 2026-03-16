783eddcd08c30eebdebd1ce354f4f38b4b3986eb
Femi John <your-email@example.com>
2026-03-16 17:14:39 +0100

    new commit

---
+++ Java/Gate-One/TaskSix.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TaskSix{
+
+    public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Enter Length: ");
+    int num1 = input.nextInt();
+
+    System.out.print("Enter Width: ");
+    int num2 = input.nextInt();
+
+    }
+}
