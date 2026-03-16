067bd996762234dfa234d5971ef37cc2424f64a9
Femi John <your-email@example.com>
2026-03-16 17:18:59 +0100

    new commit

---
+++ Java/Gate-One/TaskTwo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TaskTwo{
+
+    public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Enter Age; ");
+    int num = input.nextInt();
+
+    System.out.print(Age * 5);
+    }
+}
